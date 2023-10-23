85732f7e93fdb7235eb91df96242591db83f2f73
RudsonSantana <rudsonsanttana@gmail.com>
2023-10-23 15:48:37 -0300

    Repository - Persistence Logic

---
+++ src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
@@ -0,0 +1,19 @@
+package me.dio.credit.application.system.entity
+
+import jakarta.persistence.*
+
+@Entity
+//@Table(name = "Client")
+data class Customer(
+        @Column(nullable = false) var firstName: String = "",
+        @Column(nullable = false) var lastName: String = "",
+        @Column(nullable = false, unique = true) val cpf: String = "",
+        @Column(nullable = false, unique = true) var email: String = "",
+        @Column(nullable = false) var password: String = "",
+        @Column(nullable = false) @Embedded var address: Adress = Adress(),
+        @field:Column(nullable = false) @field:OneToMany(
+                cascade = [CascadeType.REMOVE, CascadeType.PERSIST],
+                fetch = FetchType.LAZY, mappedBy = "customer")
+        var credits: List<Credit> = mutableListOf(),
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
+)
