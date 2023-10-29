475d81ffd830178f7cd2467527dd68cb6267b09a
RudsonSantana <rudsonsanttana@gmail.com>
2023-10-29 17:50:17 -0300

    Persisting data with TestEntityManager

--- src/test/kotlin/me/dio/credit/application/system/repository/CreditRepositoryTest.kt
+++ src/test/kotlin/me/dio/credit/application/system/repository/CreditRepositoryTest.kt
@@ -1,10 +1,17 @@
 package me.dio.credit.application.system.repository
 
+import me.dio.credit.application.system.entity.Address
+import me.dio.credit.application.system.entity.Credit
+import me.dio.credit.application.system.entity.Customer
+import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import org.springframework.test.context.ActiveProfiles
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.Month
 
 @ActiveProfiles("test")
 @DataJpaTest
@@ -12,5 +19,47 @@ import org.springframework.test.context.ActiveProfiles
 class CreditRepositoryTest {
     @Autowired lateinit var creditRepository: CreditRepository
     @Autowired lateinit var  testEntityManager: TestEntityManager
-    
+
+    private lateinit var customer: Customer
+    private lateinit var credit1: Credit
+    private lateinit var credit2: Credit
+
+    @BeforeEach fun setup () {
+        customer = testEntityManager.persist(buildCustomer())
+        credit1 = testEntityManager.persist(buildCredit(customer = customer))
+        credit2 = testEntityManager.persist(buildCredit(customer = customer))
+    }
+
+    private fun buildCredit(
+        creditValue: BigDecimal = BigDecimal.valueOf(500.0),
+        dayFirstInstallment: LocalDate = LocalDate.of(2023, Month.APRIL, 22),
+        numberOfInstallment: Int = 5,
+        customer: Customer
+    ): Credit = Credit(
+        creditValue = creditValue,
+        dayFirstInstallment = dayFirstInstallment,
+        numberOfInstallment = numberOfInstallment,
+        customer = customer
+    )
+    private fun buildCustomer(
+        firstName: String = "Cami",
+        lastName: String = "Cavalcante",
+        cpf: String = "28475934625",
+        email: String = "camila@gmail.com",
+        password: String = "12345",
+        zipCode: String = "12345",
+        street: String = "Rua da Cami",
+        income: BigDecimal = BigDecimal.valueOf(1000.0),
+    ) = Customer(
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        email = email,
+        password = password,
+        address = Address(
+            zipCode = zipCode,
+            street = street,
+        ),
+        income = income,
+    )
 }
