1684bb538f880b2cadf4ad0c30094e4be771d2fc
RudsonSantana <rudsonsanttana@gmail.com>
2023-10-29 07:32:22 -0300

    Unit Test: CustomerService (save, findById and delete)

--- src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
+++ src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
@@ -6,16 +6,16 @@ import java.math.BigDecimal
 @Entity
 //@Table(name = "Client")
 data class Customer(
-        @Column(nullable = false) var firstName: String = "",
-        @Column(nullable = false) var lastName: String = "",
-        @Column(nullable = false, unique = true) var cpf: String = "",
-        @Column(nullable = false) var income: BigDecimal = BigDecimal.ZERO,
-        @Column(nullable = false, unique = true) var email: String = "",
-        @Column(nullable = false) var password: String = "",
-        @Column(nullable = false) @Embedded var address: Adress = Adress(),
-        @field:Column(nullable = false) @field:OneToMany(
+    @Column(nullable = false) var firstName: String = "",
+    @Column(nullable = false) var lastName: String = "",
+    @Column(nullable = false, unique = true) var cpf: String = "",
+    @Column(nullable = false) var income: BigDecimal = BigDecimal.ZERO,
+    @Column(nullable = false, unique = true) var email: String = "",
+    @Column(nullable = false) var password: String = "",
+    @Column(nullable = false) @Embedded var address: Address = Address(),
+    @field:Column(nullable = false) @field:OneToMany(
                 cascade = [CascadeType.REMOVE, CascadeType.PERSIST],
                 fetch = FetchType.LAZY, mappedBy = "customer")
         var credits: List<Credit> = mutableListOf(),
-        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
 )
---
+++ src/test/kotlin/me/dio/credit/application/system/service/CustomerServiceTest.kt
@@ -0,0 +1,100 @@
+package me.dio.credit.application.system.service
+
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.just
+import io.mockk.runs
+import io.mockk.verify
+import me.dio.credit.application.system.entity.Address
+import me.dio.credit.application.system.entity.Customer
+import me.dio.credit.application.system.exception.BusinessException
+import me.dio.credit.application.system.repository.CustomerRepository
+import me.dio.credit.application.system.service.impl.CustomerService
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.ActiveProfiles
+import java.math.BigDecimal
+import java.util.*
+
+@ActiveProfiles ("test")
+@ExtendWith(MockKExtension::class)
+class CustomerServiceTest {
+    @MockK lateinit var customerRepository: CustomerRepository
+    @InjectMockKs lateinit var customerService: CustomerService
+
+    @Test
+    fun `should create customer`(){
+        //given
+        val fakeCustomer: Customer = buildCustomer()
+        every { customerRepository.save(any()) } returns fakeCustomer
+        //when
+        val actual: Customer = customerService.save(fakeCustomer)
+        //then
+        Assertions.assertThat(actual).isNotNull
+        Assertions.assertThat(actual).isSameAs(fakeCustomer)
+        verify(exactly = 1) { customerRepository.save(fakeCustomer) }
+    }
+
+    @Test
+    fun `should find customer by id`() {
+        val fakeId: Long = Random().nextLong()
+        val fakeCustomer: Customer = buildCustomer(id = fakeId)
+        every { customerRepository.findById(fakeId) } returns Optional.of(fakeCustomer)
+        val actual: Customer = customerService.findById(fakeId)
+        Assertions.assertThat(actual).isNotNull
+        Assertions.assertThat(actual).isExactlyInstanceOf(Customer::class.java)
+        Assertions.assertThat(actual).isSameAs(fakeCustomer)
+        verify(exactly = 1) { customerRepository.findById(fakeId) }
+    }
+
+    @Test
+    fun `should not find customer by invalid id and throw BusinessException`() {
+        val fakeId: Long = Random().nextLong()
+        every { customerRepository.findById(fakeId) } returns Optional.empty()
+        Assertions.assertThatExceptionOfType(BusinessException::class.java)
+            .isThrownBy { customerService.findById(fakeId)}
+            .withMessage("id $fakeId not found")
+        verify(exactly = 1) { customerRepository.findById(fakeId) }
+    }
+
+    @Test
+    fun `should delete customer by id`() {
+        //given
+        val fakeId: Long = Random().nextLong()
+        val fakeCustomer: Customer = buildCustomer(id = fakeId)
+        every { customerRepository.findById(fakeId) } returns Optional.of(fakeCustomer)
+        every { customerRepository.delete(fakeCustomer) } just runs
+        //when
+        customerService.delete(fakeId)
+        //then
+        verify(exactly = 1) { customerRepository.findById(fakeId) }
+        verify(exactly = 1) { customerRepository.delete(fakeCustomer) }
+    }
+
+    fun buildCustomer(
+        firstName: String = "Rudson",
+        lastName: String = "Santana",
+        cpf: String = "12345678999",
+        email: String = "rudson@email.com",
+        password: String = "1234",
+        zipCode: String = "1234",
+        street: String = "Rua Santana",
+        income: BigDecimal = BigDecimal.valueOf(2000.0),
+        id: Long = 1L
+    ) = Customer(
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        email = email,
+        password = password,
+        address = Address(
+            zipCode = zipCode,
+            street = street,
+        ),
+        income = income,
+        id = id
+    )
+}
