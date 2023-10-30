92e28ff7db71fb245616f88781e05065f5475acf
RudsonSantana <rudsonsanttana@gmail.com>
2023-10-29 21:31:01 -0300

    Unit Test: Configuring the CreditService class

---
+++ src/test/kotlin/me/dio/credit/application/system/service/CreditServiceTest.kt
@@ -0,0 +1,36 @@
+package me.dio.credit.application.system.service
+
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.unmockkAll
+import me.dio.credit.application.system.repository.CreditRepository
+import me.dio.credit.application.system.service.impl.CreditService
+import me.dio.credit.application.system.service.impl.CustomerService
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+class CreditServiceTest {
+
+    @MockK
+    lateinit var creditRepository: CreditRepository
+
+    @MockK
+    lateinit var customerService: CustomerService
+
+    @InjectMockKs
+    lateinit var creditService: CreditService
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkAll()
+    }
+}
